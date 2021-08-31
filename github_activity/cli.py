5f132c15fae14022d8b9d57b99690885ebf6db6b
Simon Walker <s.r.walker101@googlemail.com>
2021-09-01 00:10:31 +0100

    Render bar chart

--- github_activity/cli.py
+++ github_activity/cli.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+from collections import defaultdict
 
 import click
 
@@ -41,22 +42,57 @@ def generate(owner, repository, branch, output):
 
 @main.command()
 @click.argument("input", type=click.File(mode="r"))
-def render(input):
+@click.option(
+    "-s",
+    "--sampling",
+    required=False,
+    default="3M",
+    help="Pandas-compatible resampling value",
+)
+def render(input, sampling):
     import pandas as pd
     import matplotlib.pyplot as plt
+    import numpy as np
 
     df = pd.read_json(input)
     df["createdAt"] = pd.to_datetime(df["createdAt"])
     df.sort_values(by="createdAt")
     df = df.set_index("createdAt")
 
+    unique_events = df["type"].unique()
+    bins = df.resample(sampling).count().index
+
+    xs = defaultdict(list)
+    ys = defaultdict(list)
+    for l, r in zip(bins[:-1], bins[1:]):
+        idx = (df.index >= l) & (df.index < r)
+        sample = df[idx]
+        counts = sample["type"].value_counts()
+
+        for event in unique_events:
+            try:
+                val = counts[event]
+            except KeyError:
+                val = 0
+
+            xs[event].append(l + (r - l) / 2)
+            ys[event].append(val)
+
+    bar_bottoms = np.zeros_like(xs[unique_events[0]], dtype=float)
+
     fig, axis = plt.subplots()
-    for (typ, g) in df.groupby("type"):
-        d = g.resample("1Y").count()
-        x = d.index
-        y = d["type"]
-        axis.bar(x=x.values, height=y.values, label=typ, width=1.0)
+    for key in unique_events:
+        y = np.array(ys[key]).astype(float)
+        axis.fill_between(
+            xs[key],
+            bar_bottoms,
+            y,
+            step="post",
+            label=key,
+        )
+        bar_bottoms = bar_bottoms + y
     axis.legend(loc="best")
+    axis.set(xlabel="Time", ylabel="Count")
     fig.autofmt_xdate()
     fig.tight_layout()
     plt.show()
